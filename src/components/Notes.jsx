c24e1f8d08bd400d1533733c15343ac9e8bb58f1
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-04 17:00:57 +0400

    checked index resets after theme change

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -5,9 +5,7 @@ export default function Notes(props) {
   const [noteFormActive, setNoteFormActive] = useState(false)
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
-  const [isChecked, setIsChecked] = useState(false)
-  // const [checkedIndex, setCheckedIndex] = useState([])
-  const [checkedIndex, setCheckedIndex] = useState(null)
+  const [checkedIndex, setCheckedIndex] = useState([])
   const { isDay } = props
 
   function handleCreateNote() {
@@ -16,9 +14,13 @@ export default function Notes(props) {
   }
 
   function handleCheck(index) {
-    setIsChecked(!isChecked)
-    setCheckedIndex(index)
-    console.log(checkedIndex)
+    setCheckedIndex((prevIndex) => {
+      if (Array.isArray(checkedIndex) && checkedIndex.includes(index)) {
+        setCheckedIndex(prevIndex.filter(i => i !== index))
+      } else {
+        setCheckedIndex([...prevIndex, index]) 
+      }
+    });
   }
 
   const pencilPng = (
@@ -27,7 +29,7 @@ export default function Notes(props) {
       10.5612 0.602613 10.7514 0.540824C10.9189 0.486392 11.0993 0.486392 11.2669 0.540824C11.4569 0.602571 11.6217 0.767352 11.9506 
       1.09625L13.4018 2.54738C13.7321 2.87769 13.8973 3.04292 13.9592 3.23337C14.0136 3.40088 14.0136 3.58133 13.9592 3.74885C13.8974 
       3.93916 13.7324 4.10414 13.4025 4.43398L13.4018 4.43468L11.0091 6.82736M7.67272 3.49106L11.0091 6.82736"
-       stroke="#6C63FF" stroke-linecap="round" stroke-linejoin="round"/>
+       stroke="#6C63FF" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
   )
@@ -36,10 +38,10 @@ export default function Notes(props) {
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M3.87426 7.61505C3.80724 6.74386 4.49607 6 5.36983 6H12.6302C13.504 6 14.1928 6.74385 14.1258 7.61505L13.6065 
       14.365C13.5464 15.1465 12.8948 15.75 12.1109 15.75H5.88907C5.10526 15.75 4.4536 15.1465 4.39348 14.365L3.87426 7.61505Z" stroke="#E50000"/>
-      <path d="M14.625 3.75H3.375" stroke="#E50000" stroke-linecap="round"/>
+      <path d="M14.625 3.75H3.375" stroke="#E50000" strokeLinecap="round"/>
       <path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z" stroke="#E50000"/>
-      <path d="M10.5 9V12.75" stroke="#E50000" stroke-linecap="round"/>
-      <path d="M7.5 9V12.75" stroke="#E50000" stroke-linecap="round"/>
+      <path d="M10.5 9V12.75" stroke="#E50000" strokeLinecap="round"/>
+      <path d="M7.5 9V12.75" stroke="#E50000" strokeLinecap="round"/>
     </svg>
   )
 
@@ -74,24 +76,21 @@ export default function Notes(props) {
         {notes.length? 
         (
         <div className='todo-list'>
+          {checkedIndex}
            {notes.map((note, noteIndex) => {
             return(
               <div className='note-div' key={noteIndex}> 
                 <div className='todo-text-div'>
                   <button className='checkbox'
                   style={{
-                    backgroundColor: isChecked
-                      ? (checkedIndex === noteIndex ? '#6C63FF' : '')
-                      : 'white',
+                    backgroundColor: Array.isArray(checkedIndex) && checkedIndex.includes(noteIndex) ? '#6C63FF' : 'white',
                   }}
-                  onClick={()=>{handleCheck(noteIndex)}}
-                  >
-                    {isChecked?
-                    (checkedIndex === noteIndex ? 
-                      <i className="fa-solid fa-check"></i> : "" ):(
+                  onClick={()=>{handleCheck(noteIndex)}}>
+                    {Array.isArray(checkedIndex) && checkedIndex.includes(noteIndex) ? 
+                      <i className="fa-solid fa-check"></i> :(
                       null)}
                   </button>
-                  <div className={isChecked ? (checkedIndex === noteIndex ?'checked-font' : "") : ''}> {note} </div>
+                  <div className={Array.isArray(checkedIndex) && checkedIndex.includes(noteIndex) ? 'checked-font' : ''}> {note} </div>
                 </div>
                 <div className='todo-btn-div'>
                   <button className='todo-btn edit'>{pencilPng}</button>
